2d8d6da95ee9089d65664d32a06577fa2f17bc54
RajKumarPidugu <161621633+RajKumarPidugu@users.noreply.github.com>
2024-06-28 18:19:37 -0500

    Spinner Component
    It displays a loading indicator using Bootstrap class

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,21 +1,38 @@
-import React from "react";
-import {Link} from 'react-router-dom';
-import {Form, Input} from 'antd';
+import React, {useState} from "react";
+import {Link, useNavigate} from 'react-router-dom';
+import {Form, Input, message} from 'antd';
+import axios from "axios";
+import Spinner from '../components/Layout/Spinner';
+import Password from "antd/es/input/Password";
+import { set } from "mongoose";
 const Login =() => {
+    const [Loading, setLoading] = useState(false)
+    const navigate = useNavigate()
      //from submit
-     const submitHandler = (values) => {
-        console.log(values);
+     const submitHandler = async(values) => {
+        try{
+            setLoading(true)
+            const {data} = await axios.post('/users/login', values)
+            setLoading(false)
+            message.success('login success')
+            localStorage.setItem('user', JSON.stringify({...data, Password: ''}))
+            navigate('/')
+        }   catch (error) {
+            setLoading(false)
+            message.error('something went wrong')
+        }
     };
     return (
-        <>
+    <>
         <div className="register-page">
+            {Loading && <Spinner/>  }
             <Form layout="vertical" onFinish={submitHandler}>
                 <h1>Login Form</h1>
                 <Form.Item label="Email" name="email">
                 <Input type="email" />
                 </Form.Item>
                 <Form.Item label="Password" name="password">
-                <Input type="password" />
+                <Input type="Password" />
                 </Form.Item>
                 <div className="d-flex justify-content-between">
                     <Link to="/register">Not a user? Click Here to Register</Link>
@@ -25,7 +42,7 @@ const Login =() => {
         </div>
 
         
-        </>
+    </>
     )
 }
 
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link, useNavigate} from 'react-router-dom';
 import {Form, Input, message} from 'antd';
 import axios from 'axios'
+import { set } from "mongoose";
 const Register = () => {
 const navigate = useNavigate()
+const [Loading, setLoading] = useState(false)
 
     //from submit
     const submitHandler = async (values) => {
         try{
+            setLoading(true)
           await axios.post('/users/register', values) // Here, I used Async function
           message.success('Registration Successful')
+          setLoading(false)
           navigate('/login')
         } catch(error){
+          setLoading(false)  
           message.error("invalid username or password");
         }
     };
     return (
         <>
         <div className="register-page">
+            {Loading && <Spinner />}
             <Form layout="vertical" onFinish={submitHandler}>
                 <h1>Register</h1>
                 <Form.Item label="Name" name="name">
